d16087fb6a50f4c7e5c45a5dc63971809d20d22d
Danilo Moret <github@moret.pro.br>
2015-06-25 16:00:47 -0300

    wrap jquery.lazy_content on a mixin

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
+var $ = require('jquery');
+require('./vendor/jquery.lazy_content');
+
+var METHOD_SUFIX = 'API';
+var DEFAULT_ATTR = 'data-lazy-loader';
+var DEFAULT_SELECTOR = '[' + DEFAULT_ATTR + ']';
+var DEFAULT_THRESHOLD = 200;
+
+var LazyContent = {
+  componentWillUnmount: function() {
+    this.lazyLoadersDisable();
+  },
+
+  mountLazyLoaders: function(apiMethod) {
+    this._lazyApiMethod = apiMethod;
+
+    $(DEFAULT_SELECTOR).lazyContent({
+      threshold: DEFAULT_THRESHOLD,
+      load: this._lazyLoad
+    });
+  },
+
+  lazyLoadersDisable: function() {
+    $(DEFAULT_SELECTOR).lazyContent('destroy');
+  },
+
+  _lazyLoad: function(element) {
+    var name = element.attr(DEFAULT_ATTR);
+    var apiMethod = this._lazyApiMethod || this[name + METHOD_SUFIX];
+    var onSuccess = function(data) {
+      if (!this.isMounted()) return;
+
+      var state = {};
+      state[name] = data;
+      this.setState(state);
+    }.bind(this);
+
+    apiMethod(onSuccess, name);
+  }
+};
+
+module.exports = LazyContent;
